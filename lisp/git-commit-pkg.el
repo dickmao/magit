9786cd4fc0e25bc1a26fb195ccb8893c4bc195f7
dickmao <none>
2022-02-23 10:31:27 -0500

    Revert "Reset Package-Requires for Melpa"
    This reverts commit 5ae5f958bfad89eb8e8f22fa28b11abb4b123813.

--- lisp/git-commit-pkg.el
+++ lisp/git-commit-pkg.el
@@ -1,7 +1,8 @@
 (define-package "git-commit" "3.3.0-git"
   "Edit Git commit messages."
   '((emacs "25.1")
-    (transient "20210920")
-    (with-editor "20211001"))
+    (dash "2.19.1")
+    (transient "0.3.6")
+    (with-editor "3.0.5"))
   :homepage "https://magit.vc"
   :keywords '("git" "tools" "vc"))
--- lisp/magit-section-pkg.el
+++ lisp/magit-section-pkg.el
@@ -1,6 +1,6 @@
 (define-package "magit-section" "3.3.0-git"
   "Sections for read-only buffers"
   '((emacs "25.1")
-    (dash "20210826"))
+    (dash "2.19.1"))
   :homepage "https://magit.vc"
   :keywords '("tools"))
